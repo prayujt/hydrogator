c0383e3b7b6ad355d18150ce1095a9cea1151968
Prayuj Tuli <prayujtuli@hotmail.com>
2024-10-02 15:20:16 -0400

    added unique constraints to users

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -16,6 +16,7 @@ User.init(
         email: {
             type: DataTypes.STRING,
             allowNull: false,
+            unique: true,
         },
         password: {
             type: DataTypes.STRING,
@@ -28,6 +29,7 @@ User.init(
         username: {
             type: DataTypes.STRING,
             allowNull: false,
+            unique: true,
         },
     },
     {
